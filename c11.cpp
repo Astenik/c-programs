2d3fcad610b9ab540be6c0a1c71a90a32d0a3443
Astenik <106985461+Astenik@users.noreply.github.com>
2023-12-04 22:45:29 +0400

    Update c11.cpp

--- c11.cpp
+++ c11.cpp
@@ -9,14 +9,17 @@ int main() {
     unsigned y2;
     std::cin >> x2;
     std::cin >> y2;
-    if((x1 + y1)%2 == (x2 + y2)%2)
+    if(x1 >= 1 && x1 <= 8 && x2 >= 1 && x2 <= 8  && y1 >= 1 && y1 <= 8 && y2 >= 1 && y2 <= 8)
     {
-        std::cout << "YES" << std::endl;
+        if ((x1 + y1) % 2 == (x2 + y2) % 2) {
+            std::cout << "YES" << std::endl;
+        } else {
+            std::cout << "NO" << std::endl;
+        }
     }
     else
     {
-        std::cout << "NO" << std::endl;
+        std::cout << "Invalid input" << std::endl;
     }
-    
     return 0;
-}
+    }
