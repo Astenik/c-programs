671d139ce96711c93241315f32ad263d84a84f6e
Astenik <106985461+Astenik@users.noreply.github.com>
2023-12-04 22:09:59 +0400

    Create c1.cpp

---
+++ 1.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+int main() {
+    unsigned a;
+    unsigned b;
+    std::cin >> a;
+    std::cin >> b;
+    //a
+    if(a%2 & b&2)
+    {
+        std::cout << "YES" << std::endl;
+    }
+    //b
+    if(a%2 | b&2)
+    {
+        std::cout << "YES" << std::endl;
+    }
+    //g
+    if(a%2 ^ b&2)
+    {
+        std::cout << "YES" << std::endl;
+    } 
+    //d
+    if(a%2 & b&2  | !(a%2) & !(b%2))
+    {
+        std::cout << "YES" << std::endl;
+    }
+    else //bolor depqerum
+    {
+        std::cout << "NO" << std::endl;
+    }
+    
+    return 0;
+}
